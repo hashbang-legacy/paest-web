32d0eea6b4d7a99cc0bb1fb10a77af93024cb946
Lance Vick <lvick@accesso.com>
2014-02-12 00:09:12 -0500

    initial boilerplate

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('paestApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
