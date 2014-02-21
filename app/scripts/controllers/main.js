2499270df1372e736d9c9ef509aed32ce16a7170
lrvick <lance@lrvick.net>
2014-02-20 20:26:28 -0500

    saving + auto saving :D

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('paestApp')
-  .controller('MainCtrl', function ($scope,$rootScope,$location,$routeParams) {
+  .controller('MainCtrl', function ($scope,$http,$rootScope,$location,$routeParams,$interval) {
 
     if ($routeParams.id){
       $scope.id = $routeParams.id;
@@ -19,9 +19,47 @@ angular.module('paestApp')
         $location.path('/'+id)
     }
 
-    $scope.aceLoaded = function(_editor){
+    $scope.save = function() {
+      console.log('data request', $scope.data)
+      var url = 'http://a.pae.st/'+$scope.id
+      if ( $scope.key ) url += '/'+$scope.key
+      $http({
+        url: url,
+        method: "POST",
+        data: 'p='+$scope.data,
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      }).success(function(data, status, headers, config) {
+        console.log('saved',data)
+        $scope.editUrl = data.split('\n')[2].split('#')[0]
+        $scope.key = $scope.editUrl.split('/')[4]
+      }).error(function(data, status, headers, config) {
+        console.log('not saved. stuff be brokenn',status)
+      });
+    }
+
+
+    $scope.aceLoaded = function(editor){
 
       var StatusBar = ace.require('ace/ext/statusbar').StatusBar
-      var statusBar = new StatusBar(_editor, document.getElementById('status-bar'))
+      var statusBar = new StatusBar(editor, document.getElementById('status-bar'))
+
+      var session = editor.getSession()
+
+      $scope.unsaved = false;
+      session.on("change", function(e){
+        $scope.data = session.getValue();
+        $scope.unsaved = true;
+      })
+
+      $interval(function(){
+        if ($scope.unsaved) {
+          $scope.save();
+        }
+        $scope.unsaved = false;
+      },2000)
+
+
+
     }
+
   });
